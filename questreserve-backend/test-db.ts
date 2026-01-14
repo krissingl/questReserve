b5f04de9af79ada498d1d07c4fb875f1c974bbbd
Krista Singleton <kris.singlet231@gmail.com>
2026-01-13 21:47:49 -0700

    Added test db file

---
+++ questreserve-backend/test-db.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') }); // <-- points to root .env
+
+import knex from 'knex';
+import knexConfig from './knexfile';
+
+const db = knex(knexConfig.development);
+
+db.raw('SELECT 1')
+  .then(() => console.log('Postgres connection successful!'))
+  .catch(err => console.error('Postgres connection failed:', err))
+  .finally(() => db.destroy());
