d1447e24da08ebcdfce7984db1297125bf7d94ee
Krista Singleton <kris.singlet231@gmail.com>
2026-01-19 21:29:25 -0700

    updated knex file

--- questreserve-backend/db/knexfile.ts
+++ questreserve-backend/db/knexfile.ts
@@ -1,9 +1,4 @@
 import { Knex } from 'knex';
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config();
-
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const config: { [key: string]: Knex.Config } = {
   development: {
