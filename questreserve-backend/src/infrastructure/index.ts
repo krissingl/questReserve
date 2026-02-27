f3a5a02b615ef9e0d497ae6dfac93510cbb30097
Krista Singleton <kris.singlet231@gmail.com>
2026-02-26 19:07:05 -0700

    defined base repo/infra

---
+++ questreserve-backend/src/infrastructure/index.ts
@@ -0,0 +1,10 @@
+import { Knex } from 'knex';
+
+export abstract class BaseRepository<T> {
+  constructor(protected readonly knex: Knex) {}
+
+  abstract findById(id: string): Promise<T | null>;
+  abstract findAll(): Promise<T[]>;
+  abstract create(data: Omit<T, 'id' | 'created_at' | 'updated_at'>): Promise<T>;
+  abstract update(id: string, data: Partial<Omit<T, 'id' | 'created_at' | 'updated_at'>>): Promise<T | null>;
+}
