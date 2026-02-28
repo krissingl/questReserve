0fb6079ebe8944df5f0d55161f7f28b35cb63344
Krista Singleton <kris.singlet231@gmail.com>
2026-02-28 01:06:23 -0700

    Added protected route

--- questreserve-backend/src/api/index.ts
+++ questreserve-backend/src/api/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import authRouter from './auth';
+import protectedRouter from './protected';
 
 const router = Router();
 
@@ -8,5 +9,6 @@ router.get('/health', (_req, res) => {
 });
 
 router.use('/auth', authRouter);
+router.use('/protected', protectedRouter);
 
 export default router;
